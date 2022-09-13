cc6355630542f01025f1e1862d3c9ec1fcd3f912
ReburnedBeans <100004176+ReburnedBeans@users.noreply.github.com>
2022-09-12 21:05:08 -0400

    Second half of the Flix app uploaded

---
+++ Flix_Reburn_V2/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix_Reburn_V2
+//
+//  Created by Alex Reburn on 9/12/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
